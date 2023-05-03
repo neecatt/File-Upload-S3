994066c5a06c8d15a9184bdde1ea9ee6543c0adf
HorrorOTB <nicatabdullazade135@gmail.com>
2023-05-03 10:51:49 +0400

    added space module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MulterModule } from '@nestjs/platform-express';
+import { SpacesModule } from './SpacesModule/spaces.module';
 
 @Module({
   imports: [
+    SpacesModule,
     MulterModule.registerAsync({
       useFactory: () => ({
         dest: './uploads',
